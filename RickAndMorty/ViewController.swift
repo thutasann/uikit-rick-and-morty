70d9cfaef357233bece394f31c3cabac5f555745
thutasann <thutasann@gmail.com>
2022-12-29 20:51:47 +0630

    remove storyboard

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Thuta sann on 12/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red;
+    }
+
+
+}
+
